dc96847eb93a9e8f91ecbe14a78a9b690ea28bdd
Kit Tejido <kittejido@gmail.com>
2021-05-20 23:11:20 -0600

    testing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+function BookList() {
+  return (
+    <div>
+      <Person/>
+      <Message/>
+    </div>
+  );
+}
+
+const Message = () => {
+  return <p className="">This is my message.</p>;
+};
+const Person = () => <h2>Kit</h2>;
+
+
+
+
+
+//  ### Do not touch!!!!
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BookList/>
   );
+
+
 }
 
 export default App;
